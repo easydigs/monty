a0caa2c0820ff6776fa5a874df5d365c663127e9
easydigs <easydigitalkoncept@gmail.com>
2024-01-20 18:43:07 +0100

    task 7 div

--- div.c
+++ div.c
@@ -1,9 +1,9 @@
 #include "monty.h"
 /**
- * f_div - divides the top two elements of the stack.
- * @head: stack head
- * @counter: line_number
- * Return: no return
+* f_div - divides the second top element of the stack
+* @head: stack head
+* @counter: line_number
+* Return: no return
 */
 void f_div(stack_t **head, unsigned int counter)
 {
