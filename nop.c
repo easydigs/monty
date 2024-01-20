693415fdf2b4516d0791e530e800cc0c19e75473
easydigs <easydigitalkoncept@gmail.com>
2024-01-20 18:33:07 +0100

    task 5 nop

--- nop.c
+++ nop.c
@@ -1,8 +1,8 @@
 #include "monty.h"
 /**
-  *f_nop- nothing
+  *f_nop- No operation, does nothing
   *@head: stack head
-  *@counter: line_number
+  *@counter: line_number (UNUSED)
   *Return: no return
  */
 void f_nop(stack_t **head, unsigned int counter)
